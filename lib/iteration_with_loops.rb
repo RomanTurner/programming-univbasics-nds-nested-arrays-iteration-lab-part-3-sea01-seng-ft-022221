aa3fec26935bf5f18f10d47f184aed1c73fe9b0a
Roman Turner <rn.turner@live.com>
2021-01-20 07:42:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
   while row_index < src.count do 
   element_index = 0 
   while element_index < src[row_index].count do 
-  src[row_index][element_index].is_st
+  src[row_index][element_index]
   end 
   end 
 end
